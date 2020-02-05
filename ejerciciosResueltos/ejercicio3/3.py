905872a02ce8bc2c61ef164793c3367be507bfcd
Sulfur0 <aevega1991@gmail.com>
2020-02-05 00:38:28 -0300

    terminado el tercer ejercicio

---
+++ ejerciciosResueltos/ejercicio3/3.py
@@ -0,0 +1,25 @@
+import csv
+import collections
+
+def frecuenciaPalabra(archivo):
+    cnt = collections.Counter()
+    # abris el archivo con parámetro de lectura
+    f = open(archivo, 'r')
+    for line in f:
+        for word in line.split():
+            word = word.replace(",","")
+            word = word.replace(".","")            
+            cnt[word] += 1 
+    f.close()
+    # Abrís el archivo en modo escritura
+    f = open('frecuencias.csv', 'w')
+    for elm in cnt:
+        #for word in line.split():
+        linea = elm+","+str(cnt[elm])
+        print (linea)
+        f.write(linea+'\n')
+    #return cnt
+    f.close()
+def main():
+    frecuenciaPalabra('cuentos.txt')
+main()
